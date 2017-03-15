7a32427958a0741d648480229369ed979aaf90fd
tomhon <tomhon@hotmail.com>
2017-03-14 18:45:22 -0700

    added prototype data

--- app/components/Jammers.js
+++ app/components/Jammers.js
@@ -1,5 +1,30 @@
 var React = require('react');
 
+var jammer = new Array();
+
+jammer[0] = {
+  "image": null,
+  "name": "Tom",
+  "badge": ":-)"
+}
+
+jammer[1] = {
+  "image": null,
+  "name": "Luis",
+  "badge": ":-)"
+}
+
+jammer[2] = {
+  "image": null,
+  "name": "Simon",
+  "badge": ":-)"
+}
+
+jammer[3] = {
+  "image": null,
+  "name": "Natalia",
+  "badge": ":-)"
+}
 
 
 
@@ -11,20 +36,20 @@ class Board extends React.Component {
     return (
       <div>
         <h1>Jammers</h1>
-        <div className="board-row">
-          {0}
-          {1}
-          {2}
+        <div className="jammer-row">
+          {jammer[0].image}
+          {jammer[0].name}
+          {jammer[0].badge}
         </div>
-        <div className="board-row">
-          {3}
-          {4}
-          {5}
+        <div className="jammer-row">
+          {jammer[1].image}
+          {jammer[1].name}
+          {jammer[1].badge}
         </div>
-        <div className="board-row">
-          {6}
-          {7}
-          {8}
+        <div className="jammer-row">
+          {jammer[2].image}
+          {jammer[2].name}
+          {jammer[2].badge}
         </div>
         <div>
         <img src="https://i1.sndcdn.com/avatars-000294022506-21vz5w-t500x500.jpg"></img>
--- build/transformed.js
+++ build/transformed.js
@@ -9827,8 +9827,7 @@ class Board extends React.Component {
         this.renderSquare(6),
         this.renderSquare(7),
         this.renderSquare(8)
-      ),
-      React.createElement("img", { src: "https://i1.sndcdn.com/avatars-000294022506-21vz5w-t500x500.jpg" })
+      )
     );
   }
 }
@@ -9947,6 +9946,32 @@ module.exports = Game;
 
 var React = __webpack_require__(33);
 
+var jammer = new Array();
+
+jammer[0] = {
+  "image": null,
+  "name": "Tom",
+  "badge": ":-)"
+};
+
+jammer[1] = {
+  "image": null,
+  "name": "Luis",
+  "badge": ":-)"
+};
+
+jammer[2] = {
+  "image": null,
+  "name": "Simon",
+  "badge": ":-)"
+};
+
+jammer[3] = {
+  "image": null,
+  "name": "Natalia",
+  "badge": ":-)"
+};
+
 // ===========================================================
 class Board extends React.Component {
 
@@ -9962,28 +9987,28 @@ class Board extends React.Component {
       ),
       React.createElement(
         "div",
-        { className: "board-row" },
-        0,
-        1,
-        2
+        { className: "jammer-row" },
+        jammer[0].image,
+        jammer[0].name,
+        jammer[0].badge
       ),
       React.createElement(
         "div",
-        { className: "board-row" },
-        3,
-        4,
-        5
+        { className: "jammer-row" },
+        jammer[1].image,
+        jammer[1].name,
+        jammer[1].badge
       ),
       React.createElement(
         "div",
-        { className: "board-row" },
-        6,
-        7,
-        8
+        { className: "jammer-row" },
+        jammer[2].image,
+        jammer[2].name,
+        jammer[2].badge
       ),
       React.createElement(
         "div",
-        { style: "width: 100%; height: 100%;" },
+        null,
         React.createElement("img", { src: "https://i1.sndcdn.com/avatars-000294022506-21vz5w-t500x500.jpg" })
       )
     );
